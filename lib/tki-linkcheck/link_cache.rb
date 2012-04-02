c41274b44fa6cd0fb5ada9717e406fe518d2386a
Rob McGrail <rob@cwa-rob-macbook.(none)>
2012-04-02 15:47:37 +1200

    memory fixes, and ignore sympa archives, or any pages with <base>

--- lib/tki-linkcheck/link_cache.rb
+++ lib/tki-linkcheck/link_cache.rb
@@ -1,28 +1,28 @@
 class LinkCache
   @@key = "#{$options.global_prefix}:checked"
   @@time = Time.now
-  
+
   def self.passed?(link)
     $redis.sismember @@key, link
   end
-  
-  
+
+
   def self.add(link)
     if link =~ /^http:/
       @@time = Time.now
       $redis.sadd @@key, link
     end
   end
-  
-  
+
+
   def self.flush
     recency = Time.now - @@time
     if recency > $options.linkcache_time
       $redis.del @@key
     end
   end
-  
-  
+
+
   def self.force_flush
     $redis.del @@key
   end
