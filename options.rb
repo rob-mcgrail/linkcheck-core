565e74a7bbc7a6ff2fd169d41532ac3ee8a85929
Rob McGrail <rob@cwa-rob-macbook.(none)>
2012-04-03 11:05:51 +1200

    adding global blacklist and abstracting retry for requests

--- options.rb
+++ options.rb
@@ -8,3 +8,5 @@ $options.valid_schemes = ['http', 'ftp', 'https']
 $options.checked_classes = [URI::HTTP, URI::HTTPS]
 $options.linkcache_time = 3600
 $options.page_delay = 1
+$options.retry_count = 2
+$options.permanently_ignore = [/Search=true&filter\[\]=/, /^mailto:/]
