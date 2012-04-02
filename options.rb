1bdce133ad9adac44e9172248e66f088e80f5696
Rob McGrail <rob@cwa-rob-macbook.(none)>
2012-04-02 16:58:41 +1200

    note about changing prefix option

--- options.rb
+++ options.rb
@@ -3,7 +3,9 @@ require 'ostruct'
 $options = OpenStruct.new
 
 $options.datastore = 1
-$options.global_prefix = 'tki-linkcheck'
+$options.global_prefix = 'tki-linkcheck' # changing this will orphan hundreds of redis keys.
+                                         # if the db has been used for linkchecking, try cleaning it ala:
+                                         # $ redis-cli KEYS "oldprefix*" | xargs redis-cli DEL
 $options.valid_schemes = ['http', 'ftp', 'https']
 $options.checked_classes = [URI::HTTP, URI::HTTPS]
 $options.linkcache_time = 3600
