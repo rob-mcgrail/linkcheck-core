e4c6c46e27457d68dc4ef570569cbc09759d0b63
Rob McGrail <rob@cwa-rob-macbook.(none)>
2012-04-04 16:34:10 +1200

    ajax for management of links and blacklists

--- web/public/js/script.js
+++ web/public/js/script.js
@@ -16,3 +16,23 @@ $(".targetable").hover(
     $('.' + currentId).addClass("opacitize");
   }
 );
+
+
+$("form").submit(function(event) {
+  event.preventDefault();
+  var $form = $( this ),
+  site = $form.find( 'input[name="site"]' ).val(),
+  link = $form.find( 'input[name="link"]' ).val(),
+  url = $form.attr( 'action' );
+  id = $form.attr( 'id' );
+  $.post( url, { site: site, link: link },
+    function( data ) {
+      $('#tab-pages').load('/ajax/count/pages', {site: site});
+      $('#tab-temp').load('/ajax/count/temp', {site: site});
+      $('#tab-blacklist').load('/ajax/count/blacklist', {site: site});
+    }
+  );
+  id = id.split('-');
+  id = '.row-' + id[1];
+  $(id).fadeOut();
+});
