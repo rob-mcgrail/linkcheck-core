1f4c404d01dcab2058554f27590ad9eb79964a9e
Rob McGrail <rob@cwa-rob-macbook.(none)>
2012-04-20 16:38:59 +1200

    adding sso auth module

--- lib/tki-linkcheck.rb
+++ lib/tki-linkcheck.rb
@@ -5,6 +5,7 @@ Bundler.require(:default)
 
 require './options'
 require './lib/tki-linkcheck/redis'
+require './lib/tki-linkcheck/sso_auth'
 require './lib/tki-linkcheck/check'
 require './lib/tki-linkcheck/link_cache'
 require './lib/tki-linkcheck/site'
