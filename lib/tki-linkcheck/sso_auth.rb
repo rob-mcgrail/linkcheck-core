6635149f35bb645c851f6d712fb3c2b1f5c28cbb
Rob McGrail <rob@cwa-rob-macbook.(none)>
2012-04-30 10:43:22 +1200

    Check form exists before SSO...

--- lib/tki-linkcheck/sso_auth.rb
+++ lib/tki-linkcheck/sso_auth.rb
@@ -3,11 +3,14 @@ class SSOAuth
     h = {}
     agent = Mechanize.new
     agent.get(url) do |page|
-      agent.submit(page.forms.first)
-      agent.cookies.each do |c|
-        h[c.name] = c.value
+      if page.forms.first
+        agent.submit(page.forms.first)
+        agent.cookies.each do |c|
+          h[c.name] = c.value
+        end
       end
     end
+    puts h
     h
   end
 end
