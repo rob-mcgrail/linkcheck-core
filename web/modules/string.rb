37cb58642f875de1628c6458eb0971a6a630a726
Rob McGrail <rob@cwa-rob-macbook.(none)>
2012-05-17 13:03:25 +1200

    handling slashes in site locations

--- web/modules/string.rb
+++ web/modules/string.rb
@@ -4,7 +4,7 @@ class String
   def parameterize(delimeter = '-')
     self.gsub(/[^a-z0-9\-_!?]+/i, delimeter).downcase
   end
-  
+
   def encrypt
     BCrypt::Password.create(self)
   end
@@ -12,23 +12,24 @@ class String
   def make_matchable
     BCrypt::Password.new(self)
   end
-  
+
   def to_time
     Time.at(self.to_i).strftime('%a %e %b %Y')
   end
-  
+
   def to_slug
-    self.gsub('http://', '').gsub('.', '-dot-')
+    str = self.gsub('http://', '')
+    str.gsub('.', '-dot-').gsub('/', '-slash-')
   end
-  
+
   def from_slug
-    'http://' + self.gsub('-dot-', '.')
+    'http://' + self.gsub('-dot-', '.').gsub('-slash-', '/')
   end
-  
+
   def proper_case
     self.slice(0,1).capitalize + self.slice(1..-1)
   end
-  
+
   def plural
     self + 's'
   end
