cd63b4bc60664054fc1f2c477d5f54ba927a96d1
rob.mcgrail <rob.mcgrail@5f79d30b-c411-4e78-a681-ab1a8db94be4>
2012-06-17 22:28:07 +0000

    fixing footer exclude regex
    git-svn-id: svn+ssh://svn.internal.cwa.co.nz/vol/work/infrastructure/linkcheck/trunk@15884 5f79d30b-c411-4e78-a681-ab1a8db94be4

--- options.rb
+++ options.rb
@@ -14,7 +14,7 @@ $options.permanently_ignore << /Search=true&filter\[\]=/ #search
 $options.permanently_ignore << /^mailto:/ #mailto
 $options.permanently_ignore << /\);\s?$/ #href javascript
 $options.permanently_ignore << /\/(e|m|r)\// #legacy
-$options.permanently_ignore << /www\.tki\.org\.nz\/(about|contact|help|accessibility|privacy)\// # footer
+$options.permanently_ignore << /www\.tki\.org\.nz\/(about|contact|help|accessibility|privacy)(\/|$)/ # footer
 $options.permanently_ignore << /\/wws\/arc\//
 $options.permanently_ignore << /javascript:toggle/
 $options.crawl_limit = 1500 # maximum limit of pages to crawl for one site... emergency protection against loops...
