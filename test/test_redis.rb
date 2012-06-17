95c4e6009fd6d787076467e376783d2f4eeb593c
rob.mcgrail <rob.mcgrail@5f79d30b-c411-4e78-a681-ab1a8db94be4>
2012-06-17 22:26:17 +0000

    tests for redis
    git-svn-id: svn+ssh://svn.internal.cwa.co.nz/vol/work/infrastructure/linkcheck/trunk@15771 5f79d30b-c411-4e78-a681-ab1a8db94be4

--- test/test_redis.rb
+++ test/test_redis.rb
@@ -4,17 +4,12 @@ Bundler.require(:test)
 require 'minitest/autorun'
 
 class TestRedis < MiniTest::Unit::TestCase
-  def setup
-
-  end
-
-
   def test_redis_constant_exists
-    assert_kind_of Redis, R
+    assert_kind_of Redis, $redis
   end
   
 
   def test_sever_responding
-    assert_equal 'PONG', R.ping
+    assert_equal 'PONG', $redis.ping
   end
 end
