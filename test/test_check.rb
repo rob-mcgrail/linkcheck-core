c991589487f4b23b76a42260de7986f0ab965956
rob.mcgrail <rob.mcgrail@5f79d30b-c411-4e78-a681-ab1a8db94be4>
2012-06-17 22:27:17 +0000

    tests for new linkcache behaviour
    git-svn-id: svn+ssh://svn.internal.cwa.co.nz/vol/work/infrastructure/linkcheck/trunk@15833 5f79d30b-c411-4e78-a681-ab1a8db94be4

--- test/test_check.rb
+++ test/test_check.rb
@@ -39,32 +39,47 @@ class TestCheck < MiniTest::Unit::TestCase
 
 
   def test_http_response_codes_return_correct_objects
-    stub_request(:get, "example.com/page").to_return(:status => 200)
-    assert_nil @check.validate(@doc, 'http://example.com/page')
+    stub_request(:get, "example.com/page1").to_return(:status => 200)
+    assert_nil @check.validate(@doc, 'http://example.com/page1')
 
-    stub_request(:get, "example.com/page").to_return(:status => 404)
-    assert_equal :not_found, @check.validate(@doc, 'http://example.com/page')
+    stub_request(:get, "example.com/page2").to_return(:status => 404)
+    assert_equal :not_found, @check.validate(@doc, 'http://example.com/page2')
 
-    stub_request(:get, "example.com/page").to_return(:status => 403)
-    assert_equal :forbidden, @check.validate(@doc, 'http://example.com/page')
+    stub_request(:get, "example.com/page3").to_return(:status => 403)
+    assert_equal :forbidden, @check.validate(@doc, 'http://example.com/page3')
 
-    stub_request(:get, "example.com/page").to_return(:status => 301)
-    assert_equal :moved_permanently, @check.validate(@doc, 'http://example.com/page')
+    stub_request(:get, "example.com/page4").to_return(:status => 301)
+    assert_equal :moved_permanently, @check.validate(@doc, 'http://example.com/page4')
 
-    stub_request(:get, "example.com/page").to_return(:status => 302)
-    assert_nil @check.validate(@doc, 'http://example.com/page')
+    stub_request(:get, "example.com/page5").to_return(:status => 302)
+    assert_nil @check.validate(@doc, 'http://example.com/page5')
 
-    stub_request(:get, "example.com/page").to_return(:status => 303)
-    assert_equal :see_other, @check.validate(@doc, 'http://example.com/page')
+    stub_request(:get, "example.com/page6").to_return(:status => 303)
+    assert_equal :see_other, @check.validate(@doc, 'http://example.com/page6')
 
-    stub_request(:get, "example.com/page").to_return(:status => 503)
-    assert_equal :unavailable, @check.validate(@doc, 'http://example.com/page')
+    stub_request(:get, "example.com/page7").to_return(:status => 503)
+    assert_equal :unavailable, @check.validate(@doc, 'http://example.com/page7')
 
-    stub_request(:get, "example.com/page").to_return(:status => 500)
-    assert_equal :unknown, @check.validate(@doc, 'http://example.com/page')
+    stub_request(:get, "example.com/page8").to_return(:status => 500)
+    assert_equal :unknown, @check.validate(@doc, 'http://example.com/page8')
 
-    stub_request(:get, "example.com/page").to_return(:status => 401)
-    assert_equal :unknown, @check.validate(@doc, 'http://example.com/page')
+    stub_request(:get, "example.com/page9").to_return(:status => 401)
+    assert_equal :unknown, @check.validate(@doc, 'http://example.com/page9')
+  end
+
+
+  def test_http_responses_cached
+    stub_request(:get, "example.com/page1").to_return(:status => 200)
+    assert_nil @check.validate(@doc, 'http://example.com/page1')
+
+    stub_request(:get, "example.com/page1").to_return(:status => 404)
+    assert_nil @check.validate(@doc, 'http://example.com/page1')
+
+    stub_request(:get, "example.com/page2").to_return(:status => 302)
+    assert_equal :not_found, @check.validate(@doc, 'http://example.com/page2')
+
+    stub_request(:get, "example.com/page2").to_return(:status => 200)
+    assert_equal :not_found, @check.validate(@doc, 'http://example.com/page2')
   end
 
 
