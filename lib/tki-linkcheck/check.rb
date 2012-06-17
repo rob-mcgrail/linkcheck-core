851c4d9eb5572d19ede1e218638f4afd6cb059b0
rob.mcgrail <rob.mcgrail@5f79d30b-c411-4e78-a681-ab1a8db94be4>
2012-06-17 22:26:20 +0000

    abstracting protocol types
    git-svn-id: svn+ssh://svn.internal.cwa.co.nz/vol/work/infrastructure/linkcheck/trunk@15775 5f79d30b-c411-4e78-a681-ab1a8db94be4

--- lib/tki-linkcheck/check.rb
+++ lib/tki-linkcheck/check.rb
@@ -3,12 +3,10 @@ class Check
   require 'net/http'
   
   def self.validate(page, link)
-    if link =~ /^#{Regexp.escape(page.url.to_s)}#[^!]/
+    if link =~ /^#{Regexp.escape(page.url.to_s.gsub(/\/$/,''))}\/?#[^!]/
       validate_relative_anchor(page, link)
     else
-      x = validate_link(link)
-      puts x
-      x
+      validate_link(link)
     end
   end
   
@@ -26,30 +24,30 @@ class Check
   
   
   def self.validate_link(link)
-    begin
+    if link =~ URI::regexp($options.valid_schemes)
       uri = URI.parse(link)
-    rescue URI::InvalidURIError
+      if $options.checked_schemes.member? uri.class
+        response = Net::HTTP.get_response(uri)
+        case response.code
+        when '404'
+          :not_found
+        when '403'
+          :forbidden
+        when '301'
+          :moved_permanently
+        when '302' # Should this be removed? This whole list configurable?
+          :found
+        when '303'
+          :see_other
+        when '503'
+          :unavailable
+        else
+          nil
+        end
+      end
+    else
       :invalid
     end
-    if uri.class == URI::HTTP  
-      response = Net::HTTP.get_response(uri)
-      case response.code
-      when '404'
-        :not_found
-      when '403'
-        :forbidden
-      when '301'
-        :moved_permanently
-      when '302' # Should this be removed? This whole list configurable?
-        :found
-      when '303'
-        :see_other
-      when '503'
-        :unavailable
-      else
-        nil
-      end
-    end
+
   end
-  
 end
