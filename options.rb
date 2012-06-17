fc662596ec61def7c6297cd74936ef30df960a23
rob.mcgrail <rob.mcgrail@5f79d30b-c411-4e78-a681-ab1a8db94be4>
2012-06-17 22:28:32 +0000

    adding seperate setting for check delay
    git-svn-id: svn+ssh://svn.internal.cwa.co.nz/vol/work/infrastructure/linkcheck/trunk@15905 5f79d30b-c411-4e78-a681-ab1a8db94be4

--- options.rb
+++ options.rb
@@ -7,7 +7,8 @@ $options.global_prefix = 'tki-linkcheck' # changing this will orphan hundreds of
 $options.valid_schemes = ['http', 'ftp', 'https']
 $options.checked_classes = [URI::HTTP, URI::HTTPS]
 $options.linkcache_time = 3600
-$options.page_delay = 0.5
+$options.crawl_delay = 0.5
+$options.check_delay = 0.5
 $options.retry_count = 1
 $options.permanently_ignore = [ ]
 $options.permanently_ignore << /Search=true&filter\[\]=/ #search
