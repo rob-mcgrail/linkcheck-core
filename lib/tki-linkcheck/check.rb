cc2683744a6f2bf4693d4fa79c9330572417cb34
rob.mcgrail <rob.mcgrail@5f79d30b-c411-4e78-a681-ab1a8db94be4>
2012-06-17 22:26:39 +0000

    fixes to response checking and tracking of broken pages
    git-svn-id: svn+ssh://svn.internal.cwa.co.nz/vol/work/infrastructure/linkcheck/trunk@15795 5f79d30b-c411-4e78-a681-ab1a8db94be4

--- lib/tki-linkcheck/check.rb
+++ lib/tki-linkcheck/check.rb
@@ -27,37 +27,39 @@ class Check
     if link =~ URI::regexp($options.valid_schemes)
       begin
         uri = URI.parse(link)
-        if $options.checked_classes.member? uri.class
-          begin
-            response = Net::HTTP.get_response(uri)
-            code = response.code
-          rescue
-            code = nil
-          end
-          case code
-          when '200'
-            nil
-          when '404'
-            :not_found
-          when '403'
-            :forbidden
-          when '301'
-            :moved_permanently
-          when '302' # Should this be removed?
-            nil
-          when '303'
-            :see_other
-          when '503'
-            :unavailable
-          else
-            :unknown
-          end
-        end
       rescue URI::InvalidURIError
         :invalid
       end
+      if $options.checked_classes.member? uri.class
+        begin
+          response = Net::HTTP.get_response(uri)
+          code = response.code
+        rescue
+          code = 'failed'
+        end
+        case code
+        when '200'
+          nil
+        when '404'
+          :not_found
+        when '403'
+          :forbidden
+        when '301'
+          :moved_permanently
+        when '302' # Should this be removed?
+          nil
+        when '303'
+          :see_other
+        when '503'
+          :unavailable
+        else
+          :unknown
+        end
+      else
+        :ignored
+      end
     else
-      :scheme_ignored
+      :ignored    
     end
   end
 end
