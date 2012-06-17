8ee8605a2f31acb5432104463cd9511cb30d7811
rob.mcgrail <rob.mcgrail@5f79d30b-c411-4e78-a681-ab1a8db94be4>
2012-06-17 22:27:35 +0000

    more cautious loop trigger
    git-svn-id: svn+ssh://svn.internal.cwa.co.nz/vol/work/infrastructure/linkcheck/trunk@15852 5f79d30b-c411-4e78-a681-ab1a8db94be4

--- lib/tki-linkcheck/loop_trap.rb
+++ lib/tki-linkcheck/loop_trap.rb
@@ -3,17 +3,20 @@ class LoopTrap
     $redis.incr "#{$options.global_prefix}:looptrap"
   end
 
+
   def self.count
     $redis.get "#{$options.global_prefix}:looptrap"
   end
 
+
   def self.triggered?
-    if self.count == $options.crawl_limit
+    if self.count > $options.crawl_limit
       true
     else
       nil
     end
   end
+
 
   def self.flush
     $redis.del "#{$options.global_prefix}:looptrap"
