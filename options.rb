fd634443276ce0ad27a09e18922d3883e1406224
Rob McGrail <rob.mcgrail@gmail.com>
2013-10-15 09:56:54 +1300

    Minor options tweaks.

--- options.rb
+++ options.rb
@@ -5,10 +5,10 @@ $options = OpenStruct.new
 $options.datastore = 1
 $options.global_prefix = 'tki-linkcheck' # changing this will orphan hundreds of redis keys.
 $options.expiry = 691_200
-$options.crawl_delay = 0.5
-$options.check_delay = 0.5
+$options.crawl_delay = 0.2
+$options.check_delay = 0.2
 $options.retry_count = 2
-$options.crawl_limit = 3000
+$options.crawl_limit = 5000
 
 $options.avoid = []
 $options.avoid << /\/(e|m|r)\// #legacy
@@ -19,11 +19,11 @@ $options.avoid << /\/Rate-resource\//
 $options.avoid << /\/\(redirect\)\// # mailinglist archives
 
 $options.permanently_ignore = [ ]
-$options.permanently_ignore << /Search=true&filter\[\]=/ #search
-$options.permanently_ignore << /mailto:/ #mailto
-$options.permanently_ignore << /\);\s?$/ #href javascript
-$options.permanently_ignore << /javascript:/ #href javascript
-$options.permanently_ignore << /www\.tki\.org\.nz\/(about|contact|help|accessibility|privacy|feedback)(\/|$)/ #footer
+$options.permanently_ignore << /Search=true&filter\[\]=/ # search
+$options.permanently_ignore << /mailto:/ # mailto
+$options.permanently_ignore << /\);\s?$/ # href javascript
+$options.permanently_ignore << /javascript:/ # href javascript
+$options.permanently_ignore << /www\.tki\.org\.nz\/(about|contact|help|accessibility|privacy|feedback)(\/|$)/ # footer
 $options.permanently_ignore << /wws\/arc\// # mailinglist archives
 $options.permanently_ignore << /sympa\/archive\// # mailinglist archives
 $options.permanently_ignore << /\/Rate-resource\// # weird nzc url
